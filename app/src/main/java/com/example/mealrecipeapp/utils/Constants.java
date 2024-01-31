41b317f3f1ef952193f2e5dfd3bca43ba68c8f81
Nadhif Mahardika <awandi8991@gmail.com>
2024-01-31 12:56:26 +0700

    Change Api Key

--- app/src/main/java/com/example/mealrecipeapp/utils/Constants.java
+++ app/src/main/java/com/example/mealrecipeapp/utils/Constants.java
@@ -3,5 +3,5 @@ package com.example.mealrecipeapp.utils;
 public class Constants {
     // Example constants
     public static final String API_BASE_URL = "https://api.spoonacular.com/";
-    public static final String API_KEY = "0c222c59a9c046f39f10403cd0f8a2af";
+    public static final String API_KEY = "d45c4422620d40b394d72a80d2929566";
 }
