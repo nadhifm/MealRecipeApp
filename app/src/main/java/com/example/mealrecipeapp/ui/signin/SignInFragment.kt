8a5a861911a9fa57e227db3dac946f98a49f855f
Nadhif Mahardika <awandi8991@gmail.com>
2024-02-18 19:57:39 +0700

    Add Sign In With Github

--- app/src/main/java/com/example/mealrecipeapp/ui/signin/SignInFragment.kt
+++ app/src/main/java/com/example/mealrecipeapp/ui/signin/SignInFragment.kt
@@ -14,8 +14,14 @@ import com.example.mealrecipeapp.MealRecipeApp
 import com.example.mealrecipeapp.databinding.FragmentSignInBinding
 import com.example.mealrecipeapp.ui.dialog.LoadingDialog
 import com.example.mealrecipeapp.utils.Resource
+import com.google.android.gms.tasks.OnFailureListener
+import com.google.android.gms.tasks.OnSuccessListener
+import com.google.android.gms.tasks.Task
 import com.google.android.libraries.identity.googleid.GetSignInWithGoogleOption
 import com.google.android.libraries.identity.googleid.GoogleIdTokenCredential
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.OAuthProvider
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
@@ -53,7 +59,6 @@ class SignInFragment : Fragment() {
 
         binding.signInButton.iconTint = null
         binding.signInButton.setOnClickListener {
-
             ioScope.launch {
                 val result = credentialManager.getCredential(
                     requireContext(),
@@ -71,7 +76,46 @@ class SignInFragment : Fragment() {
                 signInViewModel.saveUser(email, name, image)
             }
         }
+
+        binding.signInGithubButton.iconTint = null
+        binding.signInGithubButton.setOnClickListener {
+            signInWithGithub()
+        }
+
         observeSignInResult()
+    }
+
+    private fun signInWithGithub() {
+        val auth = FirebaseAuth.getInstance()
+
+        val provider = OAuthProvider.newBuilder("github.com")
+        provider.scopes = listOf("user:email")
+
+        // There's something already here! Finish the sign-in for your user.
+        val pendingResultTask = auth.pendingAuthResult
+        if (pendingResultTask != null) {
+            pendingResultTask
+                .addOnSuccessListener {
+                    // User is signed in.
+                    Toast.makeText(requireContext(), "User exist", Toast.LENGTH_LONG).show()
+                }
+                .addOnFailureListener {
+                    // Handle failure.
+                    Toast.makeText(requireContext(), "Error : $it", Toast.LENGTH_LONG).show()
+                }
+        } else {
+            auth.startActivityForSignInWithProvider( requireActivity(), provider.build())
+                .addOnSuccessListener {
+                    val email = it.user?.email ?: ""
+                    val name = it.user?.displayName ?: ""
+                    val image = it.user?.photoUrl.toString()
+                    signInViewModel.saveUser(email, name, image)
+                }
+                .addOnFailureListener {
+                    // Handle failure.
+                    Toast.makeText(requireContext(), "Error : $it", Toast.LENGTH_LONG).show()
+                }
+        }
     }
 
     private fun observeSignInResult() {
