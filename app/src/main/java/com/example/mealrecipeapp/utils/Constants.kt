fc04432aa95a2941f0391faa2717265c80b57019
Nadhif Mahardika <awandi8991@gmail.com>
2024-02-19 16:22:20 +0700

    Change Firebase Config and Api Key

--- app/src/main/java/com/example/mealrecipeapp/utils/Constants.kt
+++ app/src/main/java/com/example/mealrecipeapp/utils/Constants.kt
@@ -3,6 +3,6 @@ package com.example.mealrecipeapp.utils
 object Constants {
     const val HOST_NAME = "api.spoonacular.com"
     const val API_BASE_URL = "https://api.spoonacular.com/"
-    const val API_KEY = "c6102ed5dde340fd93d0958b6a320363"
+    const val API_KEY = "6ae3d89ec9674c70b31cb4eb99b20889"
     const val GOOGLE_CLIENT_ID = "804406643636-vgficofkqvdq62k249kcuqs72u4u2orv.apps.googleusercontent.com"
 }
